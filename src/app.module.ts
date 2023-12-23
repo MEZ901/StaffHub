b9b987508d2545a28ad14abeecc7edb2cc75055c
MEZ901 <issammez44@gmail.com>
2023-12-23 12:35:50 +0100

    create employee crud operators

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
